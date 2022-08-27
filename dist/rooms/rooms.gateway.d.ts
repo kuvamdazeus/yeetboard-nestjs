18d4e0d0512c616e1f8acbf4e61e217008b27076
Kuvam Bhardwaj <kuvamyash@gmail.com>
2022-08-27 21:32:08 +0530

    delete folder

--- dist/rooms/rooms.gateway.d.ts
+++
@@ -1,9 +0,0 @@
-import { RoomsService } from './rooms.service';
-import { Server, Socket } from 'socket.io';
-export declare class RoomsGateway {
-    private readonly roomsService;
-    constructor(roomsService: RoomsService);
-    server: Server;
-    handleConnection(socket: Socket): void;
-    broadcastCanvasAction(data: any, socket: Socket): void;
-}
